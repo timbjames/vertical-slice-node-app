7c9a6f6ee5f40664d689ea01e4e380d162134013
Tim James <tim.james@lrqa.com>
2025-04-07 08:18:49 +0100

    cleaning up test routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,7 @@
-import peopleRoutes from './features/people/people.routes';
-import blogsRoutes from './features/blogs/blogs.routes';
-import productsRoutes from './features/products/products.routes';
-import ordersRoutes from './features/orders/orders.routes';
 import { Router } from 'express';
 import authRoutes from './features/auth/auth.routes';
 
 const router = Router();
-
-router.use('/people', peopleRoutes);
-
-router.use('/blogs', blogsRoutes);
-
-router.use('/products', productsRoutes);
-
-router.use('/orders', ordersRoutes);
 
 router.use('/auth', authRoutes);
 //router.use('/users', userRoutes); // add more
